1e53d3a1c89c2f6343c03efe939c34da10877211
Carlos Estrada <carlos.estrada@sap-index.com>
2025-11-18 10:49:06 -0500

    UPDATE

--- sale_order_test/models/sale_order.py
+++ sale_order_test/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -16,3 +16,44 @@ class SaleOrder(models.Model):
     ],string='Método de Pago',default='credito',help='Método de pago acordado con el cliente')
 
     x_nota_cotizacion=fields.Char(string='Nota de Cotización',help='Notas adicionales para la cotización')
+
+    x_tabla_amortizacion=fields.Html(string='Tabla de Amortización',help='Detalles de la tabla de amortización para pagos a crédito',compute='_compute_tabla_amortizacion',store=False)
+
+    @api.depends('x_validez')
+    def _compute_tabla_amortizacion(self):
+        for record in self:
+            dias = 0
+
+            if record.x_validez == '7_dias':
+                dias = 7
+            elif record.x_validez == '15_dias':
+                dias = 15
+            elif record.x_validez == '30_dias':
+                dias = 30
+            
+            if dias==0:
+                record.x_tabla_amortizacion = ""
+                continue
+
+            tabla=""""
+            <table style="width: 50%; border-collapse: collapse;" border="1">
+                <tr style="background-color: #f2f2f2;">
+                    <th>Día</th>
+                    <th>Saldo</th>
+                </tr>
+            """
+
+            saldo= record.amount_total or 0.0
+            pago_diario = saldo / dias if dias > 0 else 0.0
+
+            for x in range(1, dias + 1):
+                saldo_restante = saldo - (pago_diario*x)
+                tabla += f"""
+                <tr>
+                    <td style="padding: 5px;">{x}</td>
+                    <td style="padding: 5px;">{saldo_restante:.2f}</td>
+                </tr>
+                """
+
+            tabla += "</table>"
+            record.x_tabla_amortizacion = tabla
